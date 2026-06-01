3e05ce78fd5c58e5312884842bbd5ecc0c99050c
ArturBortniczuk <a.bortniczuk@grupaeltron.pl>
2026-06-01 08:27:29 +0200

    greg

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import './index.css'; // WAŻNE: Ten import musi być PRZED App
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+// Wyłączenie logów (log, info, debug) w wersji produkcyjnej, chyba że włączono tryb debugowania w localStorage
+if (
+  process.env.NODE_ENV === 'production' &&
+  typeof window !== 'undefined' &&
+  localStorage.getItem('debug') !== 'true'
+) {
+  const noop = () => {};
+  console.log = noop;
+  console.info = noop;
+  console.debug = noop;
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
