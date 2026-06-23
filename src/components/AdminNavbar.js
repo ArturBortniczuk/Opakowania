5f35adf69ff26860db6ba928f69c8cbb8c422934
ArturBortniczuk <a.bortniczuk@grupaeltron.pl>
2026-06-23 10:09:26 +0200

    h

--- src/components/AdminNavbar.js
+++ src/components/AdminNavbar.js
@@ -82,7 +82,7 @@ const AdminNavbar = ({
                item.path !== '/admin/warehouse-drums';
       }
       if (isMagazyn) {
-        return item.path === '/admin' || item.path === '/admin/map' || item.path === '/admin/warehouse-drums' || item.path === '/admin/drums' || item.path === '/admin/returns';
+        return item.path === '/admin' || item.path === '/admin/map' || item.path === '/admin/warehouse-drums' || item.path === '/admin/drums' || item.path === '/admin/returns' || item.path === '/admin/calculator';
       }
       return true;
     })
